0c073b760c633ccf225209e8388f510e4e43f60c
nirosvaldo <150362970+nirosvaldo@users.noreply.github.com>
2023-11-24 08:38:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function enviarCadastro(event) {
+    event.preventDefault();
+    // Adicione aqui o código para processar o formulário, por exemplo, enviar para o servidor.
+    alert("Cadastro enviado! (Adicione o código de processamento)");
+  }
